18b637602bcad91326c16a2866d657a22156c602
marglodziak <marcinglodziak@gmail.com>
2023-09-08 19:40:19 +0200

    Initial commit - login + registration UI

---
+++ UI/FinanceSystemUI/src/app/components/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['../../styles/authorization.css', './register.component.css']
+})
+export class RegisterComponent {
+
+}
